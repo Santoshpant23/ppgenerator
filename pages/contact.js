3fe4c970489e6dba87f26b1cc83c0c0b4c7e505f
Santosh <pantsantosh23@gmail.com>
2022-07-12 17:06:09 +0545

    Finale

--- pages/contact.js
+++ pages/contact.js
@@ -35,8 +35,8 @@ const Contact = () => {
                         If you have any issue with this web-app or for any business related purpose then you can reach out to me either on mail or on social medias as well. All the necessary details are below: </p>  <br /> <br />
 
 
-                    <b>Email:</b>  <span className={styles.spancontact}>iamsantoshpant@gmail.com</span> <br /><br />
-                    <b>Website:</b>  <span className={styles.spaan} > <a href="https://www.santoshpant.com.np/" >santoshpant.com.np</a> </span>
+                    <p className={styles.para}>  Email:  iamsantoshpant@gmail.com  </p> <br />
+                    <p className={styles.para}>   Website: santoshpant.com.np </p>
 
 
                 </div>
